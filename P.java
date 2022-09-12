bab3960245cc60f875580bcdc3f159041d8fda89
adityarollthree <92925100+adityarollthree@users.noreply.github.com>
2022-09-12 11:52:52 +0530

    Update P.java
    The Connected Car platform is a collection of AWS cloud services,
        strategically combined to fuel innovation in the automotive industry. It
        provides fast and robust ingestion and highly reliable and durable storage
        of vehicle sensor data, simple and scalable big data services for analyzing
        the data, as well as global messaging and application services to connect
        with platform users.

--- P.java
+++ P.java
@@ -1,17 +1,4 @@
 
-info:
-  description: >
-    The Connected Car platform is a collection of AWS cloud services,
-    strategically combined to fuel innovation in the automotive industry. It
-    provides fast and robust ingestion and highly reliable and durable storage
-    of vehicle sensor data, simple and scalable big data services for analyzing
-    the data, as well as global messaging and application services to connect
-    with platform users.
-schemes:
-  - https
-paths:
-  /vehicles:
-    get:
       tags:
         - Vehicle
       summary: Retrieves list of user's vehicles
